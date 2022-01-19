66ff1db35aab830d7e7c8c47496531b7f1df8c72
Tushar Khete <tusharkhete118@gmail.com>
2022-01-19 12:09:30 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ============================== typing animation ============================ */
+var typed = new Typed(".typing",{
+    strings:["","AI Engineer","ML Expert","Blogger","Web Developer","Passionate Learner"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
